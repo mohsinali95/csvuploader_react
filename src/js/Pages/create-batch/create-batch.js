922097f3cc48559bf235497e099dc99da77cb377
mohsin-mmc <47443391+mohsin-mmc@users.noreply.github.com>
2019-09-05 06:00:29 -0700

    Upload success view

--- src/js/Pages/create-batch/create-batch.js
+++ src/js/Pages/create-batch/create-batch.js
@@ -2,38 +2,60 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Table from './table/table';
 import "./create-batch.css"
+import SuccessUpload from './success-upload/SuccessUpload';
 
 class CreateBatch extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            uploadSuccess: false
+        }
+    }
+    changeUploadStatus(value){
+        this.setState({
+            uploadSuccess: value
+        })
+    }
     render() {
-        return (
-            <div className="d-flex justify-content-center" style={{ height: "100vh" }} >
-                <div className="bg-white rounded d-flex flex-row mt-3 mb-3 cardShadow" style={{ width: '95%' }}>
-                    <div className="w-50 d-flex flex-column border border-top-0 border-left-0 border-bottom-0 m-3 align-items-center  justify-content-center">
-                        <span className="row"><h2>Upload a CSV</h2><i class="fa fa-question-circle p-2" style={{color: '#d0d0d0',fontSize: 20}}></i></span>
-                        <p>You can also download a sample template <a className="stretched-link text-success" >here</a>.</p>
-                        <button className="btn btn-success btnWidth">
-                            <i className="fa fa-cloud-upload mr-2" style={{ fontSize: 25 }} ></i>SELECT FROM COMPUTER
-                        </button>
-                        <button className="btn btn-outline-success btnWidth mt-3">
-                            SUBMIT
-                        </button>
+        let { uploadSuccess } = this.state
+
+        if (uploadSuccess) {
+            return (
+                <SuccessUpload onViewClick={this.changeUploadStatus.bind(this)}></SuccessUpload>
+            )
+        } else {
+            return (
+                <div className="d-flex justify-content-center"  >
+
+                    <div className="bg-white rounded d-flex flex-row mt-3 mb-3 cardShadow" style={{ width: '95%' }}>
+                        <div className="w-50 d-flex flex-column border border-top-0 border-left-0 border-bottom-0 m-3 align-items-center  justify-content-center">
+                            <span className="row"><h2>Upload a CSV</h2><i class="fa fa-question-circle p-2" style={{ color: '#d0d0d0', fontSize: 20 }}></i></span>
+                            <p>You can also download a sample template <a className="stretched-link text-success" >here</a>.</p>
+                            <button className="btn btn-success btnWidth">
+                                <i className="fa fa-cloud-upload mr-2" style={{ fontSize: 25 }} ></i>SELECT FROM COMPUTER
+                                    </button>
+                            <button onClick={() => { this.setState({ uploadSuccess: true }); }} className="btn btn-outline-success btnWidth mt-3">
+                                SUBMIT
+                                    </button>
+                        </div>
+                        <div className="w-50 d-flex flex-column justify-content-around  p-2">
+                            <h3>Instructions</h3>
+                            <p className="lead">
+                                1) Fields Marked * are mandatory in all situations
+                                </p>
+                            <p className="lead">
+                                2) If CNIC is selected in Transfer to, additional mandatory fields are: CNIC
+                                </p>
+                            <p className="lead">
+                                2) If Bank is selected in Transfer to, additional mandatory fields are: Bank Name & IBAN
+                                </p>
+                            <Table></Table>
+                        </div>
                     </div>
-                    <div className="w-50 d-flex flex-column justify-content-around  p-2">
-                        <h3>Instructions</h3>
-                        <p className="lead">
-                            1) Fields Marked * are mandatory in all situations
-                            </p>
-                        <p className="lead">
-                            2) If CNIC is selected in Transfer to, additional mandatory fields are: CNIC
-                            </p>
-                        <p className="lead">
-                            2) If Bank is selected in Transfer to, additional mandatory fields are: Bank Name & IBAN
-                            </p>
-                        <Table></Table>
-                    </div>
+
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 const mapStateToProps = (state) => {
---
+++ src/js/Pages/create-batch/success-upload/SuccessUpload.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+function SuccessUpload(props) {
+    let { onViewClick } = props
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh" }}>
+            <div className="bg-white rounded d-flex flex-column align-items-center justify-content-around mt-3 mb-3 cardShadow cardDiminsions" >
+                <i className="fa fa-check-circle text-success" style={{ fontSize: 150 }} ></i>
+                <h6>CSV Successfully Uploaded</h6>
+                <button onClick={() => { onViewClick(false) }} className="btn btn-success btnWidth">
+                    VIEW
+                        </button>
+            </div>
+        </div>
+    );
+}
+
+export default SuccessUpload;
