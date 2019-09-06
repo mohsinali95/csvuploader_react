ea1229fba42f87536709f6896cbd3023c0ebbf5c
mohsin-mmc <47443391+mohsin-mmc@users.noreply.github.com>
2019-09-06 06:20:34 -0700

    datatable added

--- src/js/Pages/dashboard/dashboard.js
+++ src/js/Pages/dashboard/dashboard.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Card from './card/card';
+import Datatable from './data-table/data-table';
+import "./dashboard.css"
 
 class Dashboard extends React.Component {
     render() {
         return (
-            <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh"}} >
-                <div className="card-deck">
-                    <Card title={"Completed"} number={"2529"}></Card>
-                    <Card title={"Auditor Review"} number={"15"}></Card>
-                    <Card title={"Authority Review"} number={"02"}></Card>
-                    <Card title={"Disapproved"} number={"50"}></Card>
+            <div className="d-flex flex-column justify-content-around align-items-center dashboard" style={{ height: "100vh" }} >
+                <div className='cardDiv'>
+                    <div className="card-deck">
+                        <Card title={"Completed"} number={"2529"}></Card>
+                        <Card title={"Auditor Review"} number={"15"}></Card>
+                        <Card title={"Authority Review"} number={"02"}></Card>
+                        <Card title={"Disapproved"} number={"50"}></Card>
+                    </div>
+                </div>
+                <div className="datatableShadow d-flex flex-column pl-3 pr-3 justify-content-center">
+                    <Datatable></Datatable>
                 </div>
             </div>
         );
